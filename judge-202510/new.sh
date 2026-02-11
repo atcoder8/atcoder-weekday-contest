c62fd03761ebb93ea927b8e97891f541881eee4f
pencoder <atcoder8@gmail.com>
2026-02-11 22:12:37 +0900

    2025年10月のジャッジアプデートに対応したRust用の開発環境を構築しました。

---
+++ judge-202510/new.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Pathname of the directory for the AtCoder contest.
+readonly CONTEST_DIR_PREFIX="$HOME/contests/atcoder-weekday-contest/judge-202510"
+
+if [ $# -ne 1 ]; then
+  echo "Usage: ./new <CONTEST ID>"
+  exit 1
+fi
+
+cd $CONTEST_DIR_PREFIX
+
+# Get contest name from command line argument.
+readonly CONTEST_NAME=$1
+
+# Add a package for the contest to the workspace.
+sed -i "s/^\]$/  \"$CONTEST_NAME\",\n\]/" "$CONTEST_DIR_PREFIX/Cargo.toml"
+
+# Create a package for the contest.
+cargo compete new $CONTEST_NAME --open
+
+code $CONTEST_NAME/src/bin/*.rs
